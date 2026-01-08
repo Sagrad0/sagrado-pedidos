09c06e8f3cc38380b9cf4569cb88db39a2004554
Sagrad0 <sagrado203@gmail.com>
2026-01-08 10:54:29 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,19 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  // Next 14 já usa appDir automaticamente quando existe /app
+  transpilePackages: ['undici'],
+  webpack: (config, { isServer }) => {
+    // Garante que no client o resolver prefira builds "browser" quando existirem
+    if (!isServer) {
+      config.resolve.conditionNames = ['browser', 'import', 'module', 'require', 'default']
+    }
+    return config
   },
 }
 
